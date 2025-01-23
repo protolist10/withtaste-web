ce35fb2e9febd23329262cc0deb011ccf844134e
vdementiev <vdementiev@unitedthinkers.com>
2025-01-23 20:09:24 +0200

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1745762230"
+    serviceWorkerVersion: "3173984722"
   }
 });
